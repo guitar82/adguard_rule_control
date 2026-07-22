84bc98a02e858c437518b3108a40164ffc20784d
guitar82 <mikebodell@gmail.com>
2026-07-22 13:10:04 -0400

    Release v0.1.0 (#5)

--- custom_components/adguard_rule_control/const.py
+++ custom_components/adguard_rule_control/const.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 DOMAIN = "adguard_rule_control"
 NAME = "AdGuard Rule Control"
-VERSION = "0.1.0-beta.8"
+VERSION = "0.1.0"
 
 PLATFORMS = ["switch", "binary_sensor", "sensor", "button"]
 
